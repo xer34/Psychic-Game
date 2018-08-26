75c692dab81e79111d050dad01d7374df052a1f2
Xer34 <experimental@gmail.com>
2018-08-26 06:07:21 -0400

    easy one finished, now to try hangman oh god.

--- game.js
+++ game.js
@@ -1,11 +1,14 @@
 var wins = 0;
 var losses = 0;
-var guesses = 10;
+var guesses;
 var guessesArray = [];
+var remainingGuess = 10;
 
 var winsText = document.getElementById("winsText");
 var lossesText = document.getElementById("lossesText");
 var guessesText = document.getElementById("guessesText");
+var remainingGuessText = document.getElementById("remainingGuessText");
+var guessesArrayText = document.getElementById("guessesArrayText");
 
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
@@ -16,13 +19,22 @@ document.onkeyup = function(event) {
     var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
     if (userGuess === computerGuess) {
-        wins++; }
-    else if (userGuess != computerGuess) {
-        losses++, guesses--; }
-    
+        wins ++; remainingGuess = 10; }
+    if (userGuess != computerGuess) {
+        remainingGuess -- }
+    if (remainingGuess <=0) {
+        losses ++; 
+        remainingGuess = 10;
+        guessesArray = [];  }
+    if (userGuess != computerGuess) {
+        guessesArray.push(userGuess) }
+ 
+        
     guessesText.textContent = "You chose: " + userGuess;
     winsText.textContent = "wins: " + wins;
     lossesText.textContent = "losses: " + losses;
+    remainingGuessText.textContent = "Remaining Guesses: " + remainingGuess;
+    guessesArrayText.textContent = "Your Guesses: " + guessesArray;
 
-    }
+}
 
